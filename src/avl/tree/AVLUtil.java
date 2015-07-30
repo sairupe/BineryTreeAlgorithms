cd67b8731ffc012414846bb0f07f1fa3f9891b60
SyrianaZh <260789672@qq.com>
2015-07-30 18:09:08 +0800

    删除DEBUG代码

--- src/avl/tree/AVLUtil.java
+++ src/avl/tree/AVLUtil.java
@@ -6,9 +6,6 @@ public class AVLUtil {
 	
 	public static void buildAVLTree(int[] array){
 		for(int node : array){
-			if(node == 19){
-				System.out.println();
-			}
 			AVLInsert.avlInsert2(null, AVLTreeRootNode.avlRoot , node);
 		}
 		System.out.println("================>>构建带高度的AVL树完成");
