fce835fa448ba96ec0a1d0182aa83d18fd1ab6ce
SyrianaZh <260789672@qq.com>
2015-08-03 16:12:58 +0800

    【修改】插入操作旋转根节点参数标识

--- src/redblack/tree/RedBlackTreeInsert.java
+++ src/redblack/tree/RedBlackTreeInsert.java
@@ -66,12 +66,12 @@ public class RedBlackTreeInsert {
 				// CASE 2 
 				else if(x == x.parent.rightChild){ // X是父亲的右孩子
 					x = (RedBlackTreeNode) x.parent;// 指向父节点
-					RedBlackTreeUtil.leftRotate2(x);// 父节点进行 LL 旋转
+					RedBlackTreeUtil.leftRotate2(x, RedBlackTreeRootNode.P168);// 父节点进行 LL 旋转
 				}
 				// CASE 3
 				((RedBlackTreeNode)x.parent).color = Color.BLACK;// 父节点置黑
 				((RedBlackTreeNode)x.parent.parent).color = Color.RED;// 爷节点置红
-				RedBlackTreeUtil.rightRotate2(x.parent.parent);
+				RedBlackTreeUtil.rightRotate2(x.parent.parent, RedBlackTreeRootNode.P168);
 				
 			}
 			// 左右相反的情况
@@ -87,19 +87,23 @@ public class RedBlackTreeInsert {
 				// CASE 2 
 				else if(x == x.parent.leftChild){ // X是父亲的右孩子
 					x = (RedBlackTreeNode) x.parent;// 指向父节点
-					RedBlackTreeUtil.rightRotate2(x);// 父节点进行 LL 旋转
+					RedBlackTreeUtil.rightRotate2(x, RedBlackTreeRootNode.P168);// 父节点进行 LL 旋转
 				}
 				// CASE 3
 				((RedBlackTreeNode)x.parent).color = Color.BLACK;// 父节点置黑
 				((RedBlackTreeNode)x.parent.parent).color = Color.RED;// 爷节点置红
-				RedBlackTreeUtil.leftRotate2(x.parent.parent);
+				RedBlackTreeUtil.leftRotate2(x.parent.parent, RedBlackTreeRootNode.P168);
 			}
 		}
 		((RedBlackTreeNode)RedBlackTreeRootNode.rootNodeP168).color = Color.BLACK;// 根节点置黑
 	}
 	
 	public static void main(String[] args){
+		System.out.println("==================插入前=========================");
+		RedBlackTreeUtil.RB_LDR(RedBlackTreeRootNode.rootNodeP168);
+		System.out.println("");
 		redBlackTreeInsert(RedBlackTreeRootNode.rootNodeP168, 4);
+		System.out.println("==================插入后=========================");
 		RedBlackTreeUtil.RB_LDR(RedBlackTreeRootNode.rootNodeP168);
 	}
 }
